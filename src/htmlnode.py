f3e646dc7e9b9b2fa9c029fc0696962f4cbe76c3
maxffeater <max.bont@gmail.com>
2024-06-15 15:14:02 +0200

    Ch2 - 4. Leafnode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,5 +1,5 @@
 class HTMLNode:
-    def __init__(self, tag=None, value=None, children=None, props=None):
+    def __init__(self, value=None, tag=None, children=None, props=None):
         self.tag = tag
         self.value = value
         self.children = children
@@ -16,3 +16,18 @@ class HTMLNode:
     
     def __repr__(self):
         return f"HTMLNode({self.tag},{self.value},{self.children},{self.props})"
+    
+
+class LeafNode(HTMLNode):
+    def __init__(self, value, tag=None, props=None):
+        if value is None:
+            raise ValueError("LeafNode must have a value.")
+        super().__init__(value=value, tag=tag, props=props, children=[])
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("LeafNode must have a value.")
+        elif self.tag is None:
+            return self.value
+        else:
+            return f'<{self.tag}{super().props_to_html()}>{self.value}</{self.tag}>'
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,12 +1,15 @@
 import unittest
 
 from htmlnode import HTMLNode
+from htmlnode import LeafNode
+
+
 
 
 class TestHTMLNode(unittest.TestCase):
     def test_props_to_html(self):
-        html1 = HTMLNode("a","Ceci est un lien",None,{"href": "https://www.google.com", "target": "_blank"})
-        html2 = HTMLNode("h1","Ceci est un titre",None,None)
+        html1 = HTMLNode("Ceci est un lien","a",None,{"href": "https://www.google.com", "target": "_blank"})
+        html2 = HTMLNode("Ceci est un titre","h1",None,None)
 
         # What output do you expect for html1's properties?
         expected_html1_props = ' href="https://www.google.com" target="_blank"'
@@ -17,6 +20,22 @@ class TestHTMLNode(unittest.TestCase):
         self.assertEqual(html1.props_to_html(), expected_html1_props)
         # And html2's?
         self.assertEqual(html2.props_to_html(), expected_html2_props)
+
+class TestLeafNode(unittest.TestCase):
+    def test_to_html(self):
+        leaf1 = LeafNode("Ceci est un lien","a",{"href": "https://www.google.com", "target": "_blank"})
+        leaf2 = LeafNode("Ceci est un paragraphe","p")
+        leaf3 = LeafNode("Ceci n'est pas une balise")
+
+        expected_leaf1 = '<a href="https://www.google.com" target="_blank">Ceci est un lien</a>'
+        expected_leaf2 = '<p>Ceci est un paragraphe</p>'
+        expected_leaf3 = 'Ceci n\'est pas une balise'
+
+        self.assertEqual(leaf1.to_html(), expected_leaf1)
+        self.assertEqual(leaf2.to_html(), expected_leaf2)
+        self.assertEqual(leaf3.to_html(), expected_leaf3)
+
+
 
 if __name__ == "__main__":
     unittest.main()
